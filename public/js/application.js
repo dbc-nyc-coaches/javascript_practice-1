d955f8bc86d7e0ddb7a0df416d7cbc0532d0dd91
n-zeplo <nzeplowitz@gmail.com>
2014-12-15 09:53:44 -0500

    Add Basic Structure to Boilerplate

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,21 @@
 $(document).ready(function() {
+  var question_counter = 0;
+
+  $('nav .user_authenticate').click(function(event){
+    event.preventDefault();
+
+    $('#login_modal').hide();
+    $('#signup_modal').hide();
+    $('#block_screen').show();
+    $('#'+$(this).attr('id')+'_modal').show();
+  });
+
+  $('#block_screen').click(function(event){
+    event.preventDefault();
+
+    $('#block_screen').hide();
+    $('#login_modal').hide();
+    $('#signup_modal').hide();
+  });
 
 });
